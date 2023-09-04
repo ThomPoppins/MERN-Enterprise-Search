2aea4dbc6da29b28cc4e1d68fe05e79e06d799e6
Thom Poppins <thompoppins@gmail.com>
2023-09-04 08:32:31 +0100

    Import Mongoose and start server when database is connected.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,26 @@
 import express from "express";
-import { PORT } from "./config.js";
+import { PORT, mongoDBURL } from "./config.js";
+import mongoose from "mongoose";
 
 const app = express();
 
+// GET method available at "/"
 app.get("/", (request, response) => {
   response.status(200).send("Welcome to my Express.js backend server!");
 });
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
-});
+// Connect to MongoDB database
+// If connection is successful, start Express.js backend server
+mongoose
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log("App connected successfully to the database!");
+
+    // Start Express.js server and listen to port 5555
+    app.listen(PORT, () => {
+      console.log(`App is listening to port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Mongoose connect error in index.js: ", error);
+  });
