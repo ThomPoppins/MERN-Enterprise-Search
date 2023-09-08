96793b8688d9b613445144bbfe4580c2632b26a8
Thom Poppins <thompoppins@gmail.com>
2023-09-08 10:31:27 +0100

    Implement routing for books pages and render corresponding components in the src/pages directory.

---
+++ frontend/src/pages/books/CreateBook.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const CreateBook = () => {
+  return <div>CreateBook</div>;
+};
+
+export default CreateBook;
---
+++ frontend/src/pages/books/HomeBooks.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Spinner from "../../components/Spinner";
+import { Link } from "react-router-dom";
+import { AiOutlineEdit } from "react-icons/ai";
+import { BsInfoCircle } from "react-icons/bs";
+import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+
+const HomeBooks = () => {
+  const [books, setBooks] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(process.env.REACT_APP_BACKEND_URL + "/books")
+      .then((response) => {
+        setBooks(response.data.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center">
+        <h1 className="text-3xl my-8">Books List</h1>
+        <Link to="/books/create">
+          <MdOutlineAddBox className="text-sky-800 text-4xl" />
+        </Link>
+      </div>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <table className="w-full border-separate border-spacing-2">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 rounded-md">No</th>
+            </tr>
+          </thead>
+          <tbody></tbody>
+        </table>
+      )}
+    </div>
+  );
+};
+
+export default HomeBooks;
