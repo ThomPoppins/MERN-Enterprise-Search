31e2042e61a1dd60bc172a0330bad8d66c897ef2
Thom Poppins <thompoppins@gmail.com>
2023-09-08 12:45:29 +0100

    Implement additional features in the ShowBook component

--- frontend/src/pages/books/ShowBook.jsx
+++ frontend/src/pages/books/ShowBook.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import BackButton from "../../components/BackButton";
+import Spinner from "../../components/Spinner";
+import { BACKEND_URL } from "../../../config.js";
 
 const ShowBook = () => {
+  const [book, setBook] = useState();
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(BACKEND_URL + `/books/${id}`)
+      .then((response) => {
+        setBook(response.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
+  }, []);
+
   return <div>ShowBook</div>;
 };
 
