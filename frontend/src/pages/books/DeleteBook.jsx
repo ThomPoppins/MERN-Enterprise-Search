1f719b80e096e61ff6ac7e527c39ff93c88de5e4
Thom Poppins <thompoppins@gmail.com>
2023-09-09 10:56:21 +0100

    Create pages for editting and deleting books.

--- frontend/src/pages/books/DeleteBook.jsx
+++ frontend/src/pages/books/DeleteBook.jsx
@@ -1,7 +1,47 @@
-import React from "react";
+import React, { useState } from "react";
+import BackButton from "../../components/BackButton";
+import Spinner from "../../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { BACKEND_URL } from "../../../config.js";
 
 const DeleteBook = () => {
-  return <div>DeleteBook</div>;
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  // useNavigate is a hook that allows us to navigate to a different page
+  const navigate = useNavigate();
+
+  const handleDeleteBook = () => {
+    setLoading(true);
+    axios
+      .delete(BACKEND_URL + `/books/${id}`)
+      .then(() => {
+        setLoading(false);
+        navigate("/books");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("Error deleting book, please check the console.");
+        console.log(error);
+      });
+  };
+  return (
+    <div className="p-4">
+      <BackButton destination={"/books"} />
+      <h1 className="text-3xl my-4">Delete Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col items-center border-2 border-sky-400 rounded-xl w-[600px] p-8 mx-auto">
+        <h3 className="text-2xl">Are you sure you want to delete this book?</h3>
+        <button
+          className="p-4 bg-red-600 text-white m-8 w-full"
+          onClick={handleDeleteBook}
+        >
+          Yes, delete it!
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default DeleteBook;
--- frontend/src/pages/books/EditBook.jsx
+++ frontend/src/pages/books/EditBook.jsx
@@ -1,7 +1,102 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import BackButton from "../../components/BackButton";
+import Spinner from "../../components/Spinner";
+import axios from "axios";
+import { useNavigate, useParams } from "react-router-dom";
+import { BACKEND_URL } from "../../../config.js";
 
 const EditBook = () => {
-  return <div>EditBook</div>;
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [publishYear, setPublishYear] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { id } = useParams();
+
+  // useNavigate is a hook that allows us to navigate to a different page
+  const navigate = useNavigate();
+
+  // useEffect() is a hook that runs a function when the component is rendered
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(BACKEND_URL + "/books/" + id)
+      .then((response) => {
+        setLoading(false);
+        const book = response.data;
+        setTitle(book.title);
+        setAuthor(book.author);
+        setPublishYear(book.publishYear);
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("Error fetching book, please check the console.");
+        console.log(error);
+      });
+  }, []);
+
+  // handleEditBook is a function that sends a PUT request to the backend to update a book
+  const handleEditBook = () => {
+    const data = {
+      title,
+      author,
+      publishYear,
+    };
+    setLoading(true);
+    axios
+      .put(BACKEND_URL + `/books/${id}`, data)
+      .then(() => {
+        setLoading(false);
+        navigate("/books");
+      })
+      .catch((error) => {
+        setLoading(false);
+        alert("Error creating book, please check the console.");
+        console.log(error);
+      });
+  };
+
+  return (
+    <div className="p-4">
+      <BackButton destination={"/books"} />
+      <h1 className="text-3xl my-4">Edit Book</h1>
+      {loading ? <Spinner /> : ""}
+      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Title</label>
+          <input
+            type="text"
+            value={title}
+            // onChange is a function that takes an event as an argument
+            // and sets the title state to the value of the input
+            // e.target.value is the value of the input
+            onChange={(e) => setTitle(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Author</label>
+          <input
+            type="text"
+            value={author}
+            onChange={(e) => setAuthor(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <div className="my-4">
+          <label className="text-xl mr-4 text-gray-500">Publish Year</label>
+          <input
+            type="number"
+            value={publishYear}
+            onChange={(e) => setPublishYear(e.target.value)}
+            className="border-2 border-gray-500 px-4 py-2 w-full"
+          />
+        </div>
+        <button className="p-2 bg-sky-300 m-8" onClick={handleEditBook}>
+          Save
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default EditBook;
