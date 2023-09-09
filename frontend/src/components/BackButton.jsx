83803806b5ff90879fa0ecdea80ddfb696711e23
Thom Poppins <thompoppins@gmail.com>
2023-09-09 09:31:29 +0100

    Change destination after clicking BackButton component to /books

--- frontend/src/components/BackButton.jsx
+++ frontend/src/components/BackButton.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { BsArrowLeft } from "react-icons/bs";
 
-const BackButton = ({ destination = "/" }) => {
+const BackButton = ({ destination = "/books" }) => {
   return (
     <div className="flex">
       <Link
