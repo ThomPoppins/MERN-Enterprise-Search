acee2e5b17d2b97f0b16fc119183c8d074abf5fe
Thom Poppins <thompoppins@gmail.com>
2023-09-09 09:46:51 +0100

    Write JSX for displaying book details on /books/:id page.

--- frontend/src/pages/books/ShowBook.jsx
+++ frontend/src/pages/books/ShowBook.jsx
@@ -6,7 +6,7 @@ import Spinner from "../../components/Spinner";
 import { BACKEND_URL } from "../../../config.js";
 
 const ShowBook = () => {
-  const [book, setBook] = useState();
+  const [book, setBook] = useState({});
   const [loading, setLoading] = useState(false);
   const { id } = useParams();
 
@@ -24,7 +24,42 @@ const ShowBook = () => {
       });
   }, []);
 
-  return <div>ShowBook</div>;
+  return (
+    <div className="p-4">
+      <BackButton />
+      <h1 className="text-3xl my-4">Show Book</h1>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-fit p-4">
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Id</span>
+            <span>{book._id}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Title</span>
+            <span>{book.title}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Author</span>
+            <span>{book.author}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Publish Year</span>
+            <span>{book.publishYear}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Create Time</span>
+            <span>{new Date(book.createdAt).toString()}</span>
+          </div>
+          <div className="my-4">
+            <span className="text-xl mr-4 text-gray-500">Last Update Time</span>
+            <span>{new Date(book.updatedAt).toString()}</span>
+          </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default ShowBook;
