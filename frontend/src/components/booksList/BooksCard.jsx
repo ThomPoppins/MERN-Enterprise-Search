dd55d5e4ead45ee9e7d6beba9980e65bc968533c
Thom Poppins <thompoppins@gmail.com>
2023-09-09 19:23:43 +0100

    Optimize imports by removing unused dependencies.

--- frontend/src/components/booksList/BooksCard.jsx
+++ frontend/src/components/booksList/BooksCard.jsx
@@ -1,10 +1,4 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { PiBookOpenTextLight } from "react-icons/pi";
-import { BiUserCircle } from "react-icons/bi";
-import { AiOutlineEdit } from "react-icons/ai";
-import { BsInfoCircle } from "react-icons/bs";
-import { MdOutlineDelete } from "react-icons/md";
 import BooksSingleCard from "./BooksSingleCard";
 
 const BooksCard = ({ books }) => {
