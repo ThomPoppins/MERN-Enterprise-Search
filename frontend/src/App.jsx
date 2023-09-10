f11f1ed6d0bf5894c10648f6a23a3613b86c666d
Thom Poppins <thompoppins@gmail.com>
2023-09-10 13:39:10 +0100

    Remove duplicate information from README.md.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,10 +5,12 @@ import CreateBook from "./pages/books/CreateBook";
 import EditBook from "./pages/books/EditBook";
 import ShowBook from "./pages/books/ShowBook";
 import DeleteBook from "./pages/books/DeleteBook";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
     <Routes>
+      <Route path="/" element={<Home />} />
       <Route path="/books" element={<BooksList />} />
       <Route path="/books/create" element={<CreateBook />} />
       <Route path="/books/edit/:id" element={<EditBook />} />
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home = () => {
+  return (
+    <div>
+      Visit /books and /companies route to see this application's
+      functionalities in action.
+    </div>
+  );
+};
+
+export default Home;
