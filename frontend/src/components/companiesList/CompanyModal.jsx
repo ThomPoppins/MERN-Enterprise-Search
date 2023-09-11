47411b8e03956f240a0f83b0b5c5ba51d8bbb1d8
Thom Poppins <thompoppins@gmail.com>
2023-09-11 12:20:00 +0100

    Display company name and owners in CompanyModal component.

--- frontend/src/components/companiesList/CompanyModal.jsx
+++ frontend/src/components/companiesList/CompanyModal.jsx
@@ -4,6 +4,8 @@ import { PiBookOpenTextLight } from "react-icons/pi";
 import { BiUserCircle } from "react-icons/bi";
 
 const CompanyModal = ({ company, onClose }) => {
+  const owners = company.owners.map((owner) => owner.name).join(", ");
+
   return (
     <div
       className="fixed bg-black bg-opacity-60 top-0 right-0 left-0 bottom-0 z-50 flex justify-center items-center"
@@ -26,23 +28,24 @@ const CompanyModal = ({ company, onClose }) => {
         <h4 className="my-2 text-gray-500">{company._id}</h4>
         <div className="flex justify-start items-center gap-x-2">
           <PiBookOpenTextLight className="text-red-300 text-2xl" />
-          <h2 className="my-1">{company.title}</h2>
+          <h2 className="my-1">{company.name}</h2>
         </div>
         <div className="flex justify-start items-center gap-x-2">
           <BiUserCircle className="text-red-300 text-2xl" />
-          <h2 className="my-1">{company.author}</h2>
+          <h2 className="my-1">{owners}</h2>
         </div>
-        <p className="mt-4">Anything you want to show</p>
+        <p className="mt-4">Future company slogan</p>
         <p className="my-2">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed
-          dolor felis. In ultricies, augue eget blandit rutrum, sem nisi maximus
-          metus, ut auctor magna odio sed tellus. Cras ullamcorper tristique
-          lacus, mattis lacinia velit eleifend nec. Sed est leo, vehicula sed
-          velit et, imperdiet suscipit magna. Aenean ornare vitae elit vitae
-          egestas. Quisque erat leo, lacinia at dolor et, porttitor auctor
-          ipsum. Sed pulvinar, lacus at pulvinar bibendum, nulla massa luctus
-          nisi, id dignissim magna sem eget felis. Vivamus a ex ante. Curabitur
-          a mauris ut lectus dapibus pretium.
+          Future company description: Lorem ipsum dolor sit amet, consectetur
+          adipiscing elit. Duis sed dolor felis. In ultricies, augue eget
+          blandit rutrum, sem nisi maximus metus, ut auctor magna odio sed
+          tellus. Cras ullamcorper tristique lacus, mattis lacinia velit
+          eleifend nec. Sed est leo, vehicula sed velit et, imperdiet suscipit
+          magna. Aenean ornare vitae elit vitae egestas. Quisque erat leo,
+          lacinia at dolor et, porttitor auctor ipsum. Sed pulvinar, lacus at
+          pulvinar bibendum, nulla massa luctus nisi, id dignissim magna sem
+          eget felis. Vivamus a ex ante. Curabitur a mauris ut lectus dapibus
+          pretium.
         </p>
       </div>
     </div>
