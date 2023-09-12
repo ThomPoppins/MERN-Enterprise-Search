d3cddf2587be508a88aff77195a85018da3e7369
Thom Poppins <thompoppins@gmail.com>
2023-09-12 09:53:48 +0100

    Add JWT auth functionality in backend to verify and generate JWT tokens for user authentication.

--- backend/auth/jwt.js
+++ backend/auth/jwt.js
@@ -2,12 +2,13 @@ import { JWT_SECRET } from "../config.js";
 import jwt from "jsonwebtoken";
 
 const generateToken = (user) => {
-  const { _id, name, email } = user;
+  const { _id, username, email, hashedPassword } = user;
   return jwt.sign(
     {
       _id,
       username,
       email,
+      hashedPassword,
     },
     JWT_SECRET,
     {
@@ -19,6 +20,7 @@ const generateToken = (user) => {
 const verifyToken = (token) => {
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
+    console.log("JWT verification successful: ", decoded);
     return decoded.userId;
   } catch (error) {
     console.log("JWT verification error: ", error.message);
---
+++ backend/auth/routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const { authenticateToken } = require("./middleware");
+
+const router = express.Router();
+
+/*
+ * I'm defining a protected route that requires authentication
+ * using the authenticateToken middleware function. If the JWT token is valid, the
+ * route handler will return a JSON response with a message indicating that the user is
+ * authorized to access the resource. If the JWT token is invalid or not present, the
+ * middleware function will return a 401 Unauthorized or 403 Forbidden response, and the
+ * route handler will not be called.
+ */
+router.get("/protected", authenticateToken, (req, res) => {
+  res.json({ message: "You are authorized to access this resource." });
+});
+
+module.exports = router;
