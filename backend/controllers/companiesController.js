0f0ba8ad886d910f7b198f94b42e40f11e824387
Thom Poppins <thompoppins@gmail.com>
2023-09-12 10:37:19 +0100

    Remove controller files because of CORS error, problem to be solved at a later stage.

--- backend/controllers/companiesController.js
+++
@@ -1,6 +0,0 @@
-// TODO: [MERNSTACK-148] Create a company controller
-// TODO: Create a registerCompany function in the company controller
-
-const registerCompany = async (req, res) => {};
-
-export { registerCompany };
