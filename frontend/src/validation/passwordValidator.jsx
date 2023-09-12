7dd8734b815ab32d64c6303fd5e42b824cf2c30a
Thom Poppins <thompoppins@gmail.com>
2023-09-12 13:11:37 +0100

    Add new validators for user registration.

---
+++ frontend/src/validation/passwordValidator.jsx
@@ -0,0 +1,31 @@
+const passwordValidator = (password) => {
+  // Password must be at least 8 characters long
+  if (password.length < 8) {
+    return false;
+  }
+
+  // Password must contain at least one uppercase letter
+  if (!/[A-Z]/.test(password)) {
+    return false;
+  }
+
+  // Password must contain at least one lowercase letter
+  if (!/[a-z]/.test(password)) {
+    return false;
+  }
+
+  // Password must contain at least one digit
+  if (!/\d/.test(password)) {
+    return false;
+  }
+
+  // Password must contain at least one special character
+  if (!/[^A-Za-z0-9]/.test(password)) {
+    return false;
+  }
+
+  // Password is safe
+  return true;
+};
+
+export default passwordValidator;
