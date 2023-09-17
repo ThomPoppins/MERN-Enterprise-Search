1b7128fe42c45ea84f274f73fc88f2edea53d58b
Thom Poppins <thompoppins@gmail.com>
2023-09-17 09:25:50 +0100

    Relevance order: 1. username 2. email 3. lastname 4. firstname

--- backend/utils/search/users/calculateRelevance.js
+++ backend/utils/search/users/calculateRelevance.js
@@ -1,23 +1,16 @@
 export default function calculateRelevance(user, searchTerm) {
-  const searchTerms = searchTerm.toLowerCase().split(/\s+/);
   let relevance = 0;
-
-  for (let i = 0; i < searchTerms.length; i++) {
-    const term = searchTerms[i];
-
-    if (user.username.toLowerCase().includes(term)) {
-      relevance += 4;
-    }
-    if (user.firstName.toLowerCase().includes(term)) {
-      relevance += 3;
-    }
-    if (user.lastName.toLowerCase().includes(term)) {
-      relevance += 2;
-    }
-    if (user.email.toLowerCase().includes(term)) {
-      relevance += 1;
-    }
+  if (user.username.toLowerCase().includes(searchTerm.toLowerCase())) {
+    relevance += 4;
   }
-
+  if (user.email.toLowerCase().includes(searchTerm.toLowerCase())) {
+    relevance += 3;
+  }
+  if (user.lastName.toLowerCase().includes(searchTerm.toLowerCase())) {
+    relevance += 2;
+  }
+  if (user.firstName.toLowerCase().includes(searchTerm.toLowerCase())) {
+    relevance += 1;
+  }
   return relevance;
 }
