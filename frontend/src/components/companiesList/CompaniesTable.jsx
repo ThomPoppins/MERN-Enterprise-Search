25cd49dfd4c9e69520298ee0122f229b1ec6a0ae
Thom Poppins <thompoppins@gmail.com>
2023-09-18 14:01:45 +0100

    Add modal functionalities to table view on CompaniesList page.

--- frontend/src/components/companiesList/CompaniesTable.jsx
+++ frontend/src/components/companiesList/CompaniesTable.jsx
@@ -3,8 +3,9 @@ import { Link } from "react-router-dom";
 import { AiOutlineEdit } from "react-icons/ai";
 import { BsInfoCircle } from "react-icons/bs";
 import { MdOutlineAddBox, MdOutlineDelete } from "react-icons/md";
+import CompaniesSingleRow from "./CompaniesSingleRow";
 
-const CompaniesTable = ({ companies }) => {
+const CompaniesTable = ({ companies, updateCompanies }) => {
   return (
     <table className="w-full border-separate border-spacing-2">
       <thead>
@@ -16,7 +17,7 @@ const CompaniesTable = ({ companies }) => {
             Name
           </th>
           {/* max-md:hidden hides this column on mobile devices and tablets */}
-          <th className="border border-slate-600 rounded-md w-[150px]">
+          <th className="border border-slate-600 rounded-md w-[180px]">
             Operations
           </th>
         </tr>
@@ -24,25 +25,10 @@ const CompaniesTable = ({ companies }) => {
       <tbody>
         {companies.map((company, index) => (
           <tr key={company._id} className="h-8">
-            <td className="border border-slate-700 rounded-md text-center">
-              {company.kvkNumber}
-            </td>
-            <td className="border border-slate-700 rounded-md text-left pl-3">
-              {company.name}
-            </td>
-            <td className="border border-slate-700 rounded-md text-center">
-              <div className="flex justify-center gap-x-4">
-                <Link to={`/companies/details/${company._id}`}>
-                  <BsInfoCircle className="text-2xl text-green-800" />
-                </Link>
-                <Link to={`/companies/edit/${company._id}`}>
-                  <AiOutlineEdit className="text-2xl text-yellow-600" />
-                </Link>
-                <Link to={`/companies/delete/${company._id}`}>
-                  <MdOutlineDelete className="text-2xl text-red-600" />
-                </Link>
-              </div>
-            </td>
+            <CompaniesSingleRow
+              company={company}
+              updateCompanies={updateCompanies}
+            />
           </tr>
         ))}
       </tbody>
