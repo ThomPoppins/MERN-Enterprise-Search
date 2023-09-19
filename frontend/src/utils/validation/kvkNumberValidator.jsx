405e67cc665a2ab3c61f631587c7fc947ee79d8c
Thom Poppins <thompoppins@gmail.com>
2023-09-19 10:08:47 +0100

    Add certificate chain for KVK api to gitignore

--- frontend/src/utils/validation/kvkNumberValidator.jsx
+++ frontend/src/utils/validation/kvkNumberValidator.jsx
@@ -1,3 +1,7 @@
+// Filepath to KVK API certificates related to this file
+// TODO: Before deployment to production, change the TEST certificate chain to PROD certificate chain
+const kvkApiCertificates = "./kvkApi/certs/Private_G1_chain.pem";
+
 // TODO: [MERNSTACK-189] Add real kvk number validation with API call
 const kvkNumberValidator = (kvkNumber) => {
   const regex = /^[0-9]{8}$/;
