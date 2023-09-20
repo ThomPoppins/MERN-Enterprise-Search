40cbba4dd20a9763bc1681781ad18d9ea50593da
Thom Poppins <thompoppins@gmail.com>
2023-09-20 09:32:14 +0100

    Make company slogan validator handle white spaces

--- frontend/src/utils/validation/companySloganValidator.jsx
+++ frontend/src/utils/validation/companySloganValidator.jsx
@@ -1,8 +1,8 @@
 // Validate company slogan to be alphanumeric and special characters and not empty
 function companySloganValidator(slogan) {
-  const regex = /^[a-zA-Z0-9!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
+  const regex = /^[a-zA-Z0-9\s!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]*$/;
 
-  return slogan.length > 0 && regex.test(slogan);
+  return slogan.trim().length > 0 && regex.test(slogan);
 }
 
 export default companySloganValidator;
