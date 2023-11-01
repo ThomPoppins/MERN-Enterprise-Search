d1636fdb09b18b0ca8c71c60e334017cc254f58d
Thom Veldpaus <thompoppins@gmail.com>
2023-11-01 14:15:08 +0100

    Add route to get invites from a specific user

--- backend/routes/invitesRoute.js
+++ backend/routes/invitesRoute.js
@@ -4,6 +4,27 @@ import inviteModel from "../models/inviteModel.js";
 
 const router = express.Router();
 
+// Route to get all invites from a specific user
+router.get("/:userId", async (request, response) => {
+  // Find all invites from a specific user
+  try {
+    const invites = await inviteModel.find({
+      $or: [
+        { sender: request.params.userId },
+        { recipient: request.params.userId },
+      ],
+    });
+
+    // Send status 200 response and the invites as JSON response if successful
+    return response.status(200).json(invites);
+  } catch (error) {
+    // Send status 500 response and error message as JSON response if unsuccessful
+    return response.status(500).json({
+      message: error.message,
+    });
+  }
+});
+
 // Route to save a new invite
 router.post("/", async (request, response) => {
   // Create a new invite document using the Invite model
