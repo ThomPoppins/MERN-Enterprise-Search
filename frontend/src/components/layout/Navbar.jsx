84a7143ec840216b3050f9ffe8abbba4858262ff
Thom Veldpaus <thompoppins@gmail.com>
2023-11-01 13:08:37 +0100

    Conditional render /companies link in Navbar

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -11,18 +11,27 @@ const Navbar = () => {
     <nav className="bg-gray-800 p-4 shadow-lg">
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
-          <div className="text-white text-2xl font-bold">Vind Expert</div>
+          <div className="text-white">
+            <h1 className=" text-2xl font-bold">Vind Expert</h1>
+          </div>
+          {userId && (
+            <div className="text-white">
+              <Link to="/companies">Companies</Link>
+            </div>
+          )}
           <div className="hidden md:flex space-x-4">
             {userId ? (
-              <div className="flex items-center space-x-2">
-                <img
-                  src={user?.profilePicture}
-                  alt="profile picture"
-                  className="w-8 h-8 rounded-full ml-2"
-                />
-                <Link to="/logout" className="text-white">
-                  Logout
-                </Link>
+              <div>
+                <div className="flex items-center space-x-2">
+                  <img
+                    src={user?.profilePicture}
+                    alt="profile picture"
+                    className="w-8 h-8 rounded-full ml-2"
+                  />
+                  <Link to="/logout" className="text-white">
+                    Logout
+                  </Link>
+                </div>
               </div>
             ) : (
               <div className="flex items-center space-x-2">
--- frontend/src/pages/users/LoginUser.jsx
+++ frontend/src/pages/users/LoginUser.jsx
@@ -9,6 +9,7 @@ import { useSnackbar } from "notistack";
 import emailValidator from "../../utils/validation/emailValidator";
 import verifyToken from "../../utils/auth/verifyToken.jsx";
 import store from "../../store/store.jsx";
+import Navbar from "../../components/layout/Navbar";
 
 const RegisterUser = () => {
   // Input field values for logging in a user as state
@@ -95,42 +96,45 @@ const RegisterUser = () => {
   };
 
   return (
-    <div className="p-4">
-      <BackButton destination={"/"} />
-      <h1 className="text-3xl my-4">Login</h1>
-      {loading ? <Spinner /> : ""}
-      <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Email</label>
-          <input
-            type="text"
-            value={email}
-            onChange={handleEmailChange}
-            onBlur={validateEmail}
-            className={`border-2 border-gray-500 px-4 py-2 w-full ${
-              emailError ? "border-red-500" : ""
-            }`}
-          />
-          {emailError ? (
-            <p className="text-red-500 text-sm">
-              Email must be a valid email address.
-            </p>
-          ) : (
-            ""
-          )}
+    <div>
+      <Navbar />
+      <div className="p-4">
+        <BackButton destination={"/"} />
+        <h1 className="text-3xl my-4">Login</h1>
+        {loading ? <Spinner /> : ""}
+        <div className="flex flex-col border-2 border-sky-400 rounded-xl w-[600px] p-4 mx-auto">
+          <div className="my-4">
+            <label className="text-xl mr-4 text-gray-500">Email</label>
+            <input
+              type="text"
+              value={email}
+              onChange={handleEmailChange}
+              onBlur={validateEmail}
+              className={`border-2 border-gray-500 px-4 py-2 w-full ${
+                emailError ? "border-red-500" : ""
+              }`}
+            />
+            {emailError ? (
+              <p className="text-red-500 text-sm">
+                Email must be a valid email address.
+              </p>
+            ) : (
+              ""
+            )}
+          </div>
+          <div className="my-4">
+            <label className="text-xl mr-4 text-gray-500">Password</label>
+            <input
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="border-2 border-gray-500 px-4 py-2 w-full"
+            />
+          </div>
+          <button className="p-2 bg-sky-300 m-8" onClick={handleLoginUser}>
+            Login
+          </button>
         </div>
-        <div className="my-4">
-          <label className="text-xl mr-4 text-gray-500">Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="border-2 border-gray-500 px-4 py-2 w-full"
-          />
-        </div>
-        <button className="p-2 bg-sky-300 m-8" onClick={handleLoginUser}>
-          Login
-        </button>
       </div>
     </div>
   );
