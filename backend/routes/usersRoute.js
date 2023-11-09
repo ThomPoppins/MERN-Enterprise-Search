19fbb766afdf6fcd2172a6b9177e1dde992d6d46
Thom Veldpaus <thompoppins@gmail.com>
2023-11-09 16:29:54 +0100

    Homepage upgrade and animations on the Find button

--- backend/routes/usersRoute.js
+++ backend/routes/usersRoute.js
@@ -55,15 +55,13 @@ router.post("/", async (request, response) => {
     const salt = await bcrypt.genSalt(10);
     const hashedPassword = await bcrypt.hash(request.body.password, salt);
 
-    // TODO: Make first name and last name required on user registration
-
     // Create a new user document using the User model and the properties from the request body.
     const newUser = {
       username: request.body.username,
       email: request.body.email,
       hashedPassword: hashedPassword,
-      firstName: request.body.firstName ? request.body.firstName : "",
-      lastName: request.body.lastName ? request.body.lastName : "",
+      firstName: request.body.firstName,
+      lastName: request.body.lastName,
       gender: request.body.gender,
     };
 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { BACKEND_URL } from "../../config";
 import Layout from "../components/layout/Layout";
@@ -7,8 +7,22 @@ const Home = () => {
   let userId = useSelector((state) => state.userId);
   let user = useSelector((state) => state.user);
 
+  // Spin find button (after click Find)
+  const [findButtonSpinning, setFindButtonSpinning] = useState(false);
+  const [searchQuery, setSearchQuery] = useState("");
+
   const handleFindExpertsQuery = (e) => {
-    console.log("Find experts query", e.target.value);
+    setFindButtonSpinning(true);
+
+    //! TODO: Remove this timeout when the query is implemented and redirects to the search page
+    setTimeout(() => {
+      setFindButtonSpinning(false);
+    }, 2000);
+  };
+
+  // Search query input change handler
+  const handleSearchQueryChange = (e) => {
+    setSearchQuery(e.target.value);
   };
 
   if (userId) {
@@ -34,20 +48,28 @@ const Home = () => {
                   <div className="absolute top-[120px] left-[172px] w-full mt-6 mb-3">
                     <input
                       type="text"
-                      className="absolute right-[396px] top-[-14px] h-[50px]  border-2 border-purple-900 bg-cyan-100 focus:bg-white rounded-xl text-gray-800 py-2 px-4 mt-6 w-2/3"
+                      className="absolute right-[360px] top-[-14px] h-[50px]  border-2 border-purple-900 bg-cyan-100 focus:bg-white rounded-xl text-gray-800 py-2 px-4 mt-6 w-2/3"
                       placeholder='Search for experts "plumber", "electrician", "gardener", etc."'
+                      value={searchQuery}
+                      onChange={handleSearchQueryChange}
                     />
 
                     <button
-                      className="absolute top-[-21px] right-[152px] h-[50px] w-[190px] bg-gradient-to-r hover:bg-green-400 find-button hover:bg-gradient-to-l rounded-lg animate-pulse hover:animate-none w-[120px] m-8 pl-4"
+                      className={`absolute top-[-21px] right-[164px] h-[50px] w-[160px] hover:bg-gradient-to-r bg-green-400 rounded-lg animate-pulse w-[120px] m-8 pl-4  ${
+                        findButtonSpinning
+                          ? "animate-spin"
+                          : "animate-bounce hover:animate-none"
+                      }`}
                       onClick={handleFindExpertsQuery}
                     >
                       <div className="mx-auto">
-                        <div className="py-2 text-xl float-left">Find </div>
+                        <div className="py-2 ml-7 text-xl float-left">
+                          Find{" "}
+                        </div>
                         <img
                           src={`${BACKEND_URL}/logo/vind-expert-transparent.png`}
                           alt="Vind-Expert find button"
-                          className="w-6 h-6 mt-3 mr-4 rounded-xs object-cover ml-2 float-right"
+                          className="w-5 h-5 mt-[13px] mr-8 rounded-xs object-cover ml-2 float-right"
                         />
                       </div>
                     </button>
