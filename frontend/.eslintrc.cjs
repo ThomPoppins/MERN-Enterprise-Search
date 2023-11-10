c6409a17692f0450e2ddbd90ed7193c118305ad5
Thom Veldpaus <thompoppins@gmail.com>
2023-11-10 22:36:05 +0100

    Finished basic React setup for ES Lint

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -5,7 +5,9 @@ module.exports = {
   },
   'extends': [
     "eslint:recommended",
+    // "eslint:all",
     "plugin:react/recommended",
+    // "plugin:react/all",
     "plugin:react/jsx-runtime",
     'prettier',
   ],
@@ -45,5 +47,23 @@ module.exports = {
   'rules': {
     'react/jsx-uses-react': 'error',
     'react/jsx-uses-vars': 'error',
+    'react/display-name': 'error',
+    'react/jsx-key': 'error',
+    'react/jsx-no-comment-textnodes': 'error',
+    'react/jsx-no-target-blank': 'error',
+    'react/jsx-no-undef': 'error',
+    'react/no-children-prop': 'error',
+    'react/no-danger-with-children': 'error',
+    'react/no-deprecated': 'error',
+    'react/no-direct-mutation-state': 'error',
+    'react/no-find-dom-node': 'error',
+    'react/no-is-mounted': 'error',
+    'react/no-render-return-value': 'error',
+    'react/no-string-refs': 'error',
+    'react/no-unescaped-entities': 'error',
+    'react/no-unknown-property': 'error',
+    'react/prop-types': 'error', //! BE AWARE THAT YOU NEED TO KNOW HOW TO USE PROP TYPES
+    'react/react-in-jsx-scope': 'error',
+    'react/require-render-return': 'error',
   },
 };
