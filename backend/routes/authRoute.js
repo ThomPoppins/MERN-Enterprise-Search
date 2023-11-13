254354ac516e077606ee6bfc43544f6fc766121b
Thom Veldpaus <thompoppins@gmail.com>
2023-11-14 00:35:32 +0100

    remove trailing semi's

--- backend/routes/authRoute.js
+++ backend/routes/authRoute.js
@@ -1,16 +1,16 @@
-import express from "express";
-import { verifyToken } from "../middleware/auth/jwt.js";
+import express from 'express'
+import { verifyToken } from '../middleware/auth/jwt.js'
 
-const router = express.Router();
+const router = express.Router()
 
-router.get("/verify-token", async (request, response) => {
+router.get('/verify-token', (request, response) => {
   const { token } = request.query,
-   userId = verifyToken(token);
+    userId = verifyToken(token)
   // Console.log("userId in authRoute.js: ", userId);
   if (userId) {
-    return response.status(200).json({ userId });
+    return response.status(200).json({ userId })
   }
-  return response.status(401).json({ message: "Invalid token" });
-});
+  return response.status(401).json({ message: 'Invalid token' })
+})
 
-export default router;
+export default router
