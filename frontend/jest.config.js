46dbe0ef745bbf1a22d5d84fca9e08dd36e132d5
Thom Veldpaus <thompoppins@gmail.com>
2023-11-15 18:20:18 +0100

    Fixed ImageCrop

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-implicit-coercion */
+/* eslint-disable line-comment-position */
+/* eslint-disable no-inline-comments */
+module.exports = {
+  testEnvironment: 'jest-environment-jsdom',
+  moduleDirectories: [
+    'node_modules',
+    +(
+      // add the directory with the test-utils.js file, for example:
+      (+'utils')
+    ), // a utility folder
+    +__dirname, // the root directory
+  ],
+  // ... other options ...
+}
--- frontend/src/pages/dev/ImageCrop.jsx
+++ frontend/src/pages/dev/ImageCrop.jsx
@@ -1,55 +1,132 @@
-/* eslint-disable react/jsx-props-no-spreading */
-/* eslint-disable jsx-a11y/click-events-have-key-events */
-import React, { useCallback } from 'react'
-import { useDropzone } from 'react-dropzone'
+import React, { useState, useCallback, useRef, useEffect } from 'react'
+import ReactCrop from 'react-image-crop'
+import 'react-image-crop/dist/ReactCrop.css'
+
+const generateDownload = (canvas, crop) => {
+  if (!crop || !canvas) {
+    return
+  }
+
+  canvas.toBlob(
+    (blob) => {
+      const previewUrl = window.URL.createObjectURL(blob)
+
+      const anchor = document.createElement('a')
+      anchor.download = 'cropPreview.png'
+      anchor.href = URL.createObjectURL(blob)
+      anchor.click()
+
+      window.URL.revokeObjectURL(previewUrl)
+    },
+    'image/png',
+    1,
+  )
+}
+
+const setCanvasImage = (image, canvas, crop) => {
+  if (!crop || !canvas || !image) {
+    return
+  }
+
+  const scaleX = image.naturalWidth / image.width
+  const scaleY = image.naturalHeight / image.height
+  const ctx = canvas.getContext('2d')
+  // refer https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio
+  const pixelRatio = window.devicePixelRatio
+
+  canvas.width = crop.width * pixelRatio * scaleX
+  canvas.height = crop.height * pixelRatio * scaleY
+
+  // refer https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/setTransform
+  ctx.setTransform(pixelRatio, 0, 0, pixelRatio, 0, 0)
+  ctx.imageSmoothingQuality = 'high'
+
+  // refer https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D/drawImage
+  ctx.drawImage(
+    image,
+    crop.x * scaleX,
+    crop.y * scaleY,
+    crop.width * scaleX,
+    crop.height * scaleY,
+    0,
+    0,
+    crop.width * scaleX,
+    crop.height * scaleY,
+  )
+}
 
 const ImageCrop = () => {
-  const onDrop = useCallback((acceptedFiles) => {
-    console.log('acceptedFiles: ', acceptedFiles)
+  const [upImg, setUpImg] = useState()
 
-    acceptedFiles.forEach((file) => {
-      // File reader to read the file
+  const imgRef = useRef(null)
+  const previewCanvasRef = useRef(null)
+
+  const [crop, setCrop] = useState({ unit: 'px', width: 30, aspect: 1 })
+  const [completedCrop, setCompletedCrop] = useState(null)
+
+  console.log(crop)
+
+  // on selecting file we set load the image on to cropper
+  const onSelectFile = (e) => {
+    if (e.target.files && e.target.files.length > 0) {
       const reader = new FileReader()
+      reader.addEventListener('load', () => setUpImg(reader.result))
+      reader.readAsDataURL(e.target.files[0])
+    }
+  }
 
-      // Read the file as a binary string
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsBinaryString
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onload
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onerror
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/onabort
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsArrayBuffer
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsDataURL
-      // https://developer.mozilla.org/en-US/docs/Web/API/FileReader/readAsText
-
-      // .onabort: A handler for the abort event. This event is triggered each time the reading operation is aborted.
-      reader.onabort = () => console.log('file reading was aborted')
-      // .onerror: A handler for the error event. This event is triggered each time the reading operation encounter an error.
-      reader.onerror = () => console.log('file reading has failed')
-      // .onload: A handler for the load event. This event is triggered each time the reading operation is successfully completed.
-      reader.onload = () => {
-        // Binary string of the file contents
-        const binaryStr = reader.result
-
-        console.log('binaryStr: ', binaryStr)
-      }
-      reader.readAsArrayBuffer(file)
-    }, [])
+  const onLoad = useCallback((img) => {
+    imgRef.current = img
   }, [])
 
-  const { getInputProps, getRootProps, isDragActive } = useDropzone({ onDrop })
+  useEffect(() => {
+    setCanvasImage(imgRef.current, previewCanvasRef.current, completedCrop)
+  }, [completedCrop])
 
   return (
-    <div
-      {...getRootProps({
-        className: 'dropzone bg-purple-500 text-white p-4 rounded-lg mt-4 mx-4',
-      })}
-    >
-      <input {...getInputProps()} />
-      {isDragActive ? (
-        <p>Drop the files here ...</p>
-      ) : (
-        <p>Drag &apos;n&apos; drop some files here, or click to select files</p>
-      )}
+    <div className='App'>
+      <div>
+        <input accept='image/*' onChange={onSelectFile} type='file' />
+      </div>
+
+      <div className='flex justify-center'>
+        <div>
+          <ReactCrop
+            className='h-[500px] w-[500px]'
+            crop={crop}
+            onChange={(c) => setCrop(c)}
+            onComplete={(c) => setCompletedCrop(c)}
+            onImageLoaded={onLoad}
+            src={upImg}
+          />
+        </div>
+        <div>
+          <canvas
+            className='rounded-full'
+            ref={previewCanvasRef}
+            // Rounding is important so the canvas width and height matches/is a multiple for sharpness.
+            style={{
+              width: '300px',
+              height: '300px',
+            }}
+          />
+        </div>
+        {/* Canvas to display cropped image */}
+      </div>
+      <p>
+        Note that the download below won't work in this sandbox due to the
+        iframe missing 'allow-downloads'. It's just for your reference.
+      </p>
+      <button
+        className='bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded'
+        disabled={!completedCrop?.width || !completedCrop?.height}
+        onClick={() =>
+          generateDownload(previewCanvasRef.current, completedCrop)
+        }
+        type='button'
+      >
+        Download cropped image
+      </button>
     </div>
   )
 }
