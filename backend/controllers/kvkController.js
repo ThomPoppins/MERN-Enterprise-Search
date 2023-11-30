1c7c78924fab5c445b219e86d208025858cc9f4c
Thom Veldpaus <thompoppins@gmail.com>
2023-11-30 17:14:21 +0100

    Fix nav link

--- backend/controllers/kvkController.js
+++ backend/controllers/kvkController.js
@@ -11,27 +11,27 @@ const PATH_TO_KVK_API_CERTIFICATE_CHAIN_RELATIVE_TO_INDEX_APP =
 export const getKvkData = async (request, response) => {
   try {
     // Get the query from the request query parameters
-    const { kvkNumber } = request.query,
-      // Get the certificate chain from the file system
-      certificateChain = fs.readFileSync(
-        PATH_TO_KVK_API_CERTIFICATE_CHAIN_RELATIVE_TO_INDEX_APP,
-        'utf8',
-      ),
-      // Create an https agent with the certificate chain
-      // https://nodejs.org/api/https.html#https_https_request_options_callback
-      agent = new https.Agent({
-        ca: certificateChain,
-      }),
-      // Get the data from the KVK API GET request
-      { data } = await axios.get(
-        `https://api.kvk.nl/test/api/v1/naamgevingen/kvknummer/${kvkNumber}`,
-        {
-          headers: {
-            apikey: KVK_TEST_API_KEY,
-          },
-          httpsAgent: agent,
+    const { kvkNumber } = request.query
+    // Get the certificate chain from the file system
+    const certificateChain = fs.readFileSync(
+      PATH_TO_KVK_API_CERTIFICATE_CHAIN_RELATIVE_TO_INDEX_APP,
+      'utf8',
+    )
+    // Create an https agent with the certificate chain
+    // https://nodejs.org/api/https.html#https_https_request_options_callback
+    const agent = new https.Agent({
+      ca: certificateChain,
+    })
+    // Get the data from the KVK API GET request
+    const { data } = await axios.get(
+      `https://api.kvk.nl/test/api/v1/naamgevingen/kvknummer/${kvkNumber}`,
+      {
+        headers: {
+          apikey: KVK_TEST_API_KEY,
         },
-      )
+        httpsAgent: agent,
+      },
+    )
 
     // Send status 200 response and the data to the client
     return response.status(200).json(data)
